d5a593adf7017724fe94da23abd6e04b0583f048
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2024-06-07 20:13:01 -0400

    done

---
+++ src/middlewares/isLogin.middleware.js
@@ -0,0 +1,9 @@
+import { request, response } from "express";
+
+export const isLogin = async (req = request, res = response, next) => {
+  if(req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ status: "Error", msg: "Usuario no logueado"});
+  }
+}
